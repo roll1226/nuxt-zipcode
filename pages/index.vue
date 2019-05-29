08ef013ed0abbba3de1cbf426a74c242642121f8
roll1226 <tomo_yasu_4724@icloud.com>
2019-05-29 18:00:14 +0900

    add/ruby

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,28 @@
 <template>
   <section class="container">
-    <input v-model="zipcode" type="text" placeholder="郵便番号を入力" />
-    <button @click="searchAddressInfo">住所自動入力</button>
-    <p>
-      住所：{{ addressData['address1'] }}{{ addressData['address2']
-      }}{{ addressData['address3'] }}
+    <label
+      >住所検索
+      <input
+        v-model="zipcode"
+        type="text"
+        placeholder="郵便番号を入力"
+        @keyup="searchAddressInfo"
+    /></label>
+
+    <!-- <br /> -->
+    <!-- <button @click="searchAddressInfo">住所自動入力</button> -->
+    <br />
+    <p v-if="zipcode === ''">
+      入力してね
     </p>
-    <p id="error">
+    <p v-else-if="msg == ''">
+      住所：<ruby
+        >{{ addressData['address1'] }}<rt>{{ addressData['kana1'] }}</rt
+        >{{ addressData['address2'] }}<rt>{{ addressData['kana2'] }}</rt
+        >{{ addressData['address3'] }}<rt>{{ addressData['kana3'] }}</rt></ruby
+      >
+    </p>
+    <p v-else id="error">
       {{ msg }}
     </p>
   </section>
@@ -38,6 +54,10 @@ export default {
           this.addressData = res.data.results[0]
           this.msg = ''
         }
+        if (this.zipcode === '') {
+          this.addressData = ''
+          this.msg = ''
+        }
       })
     }
   }
@@ -45,6 +65,14 @@ export default {
 </script>
 
 <style>
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+}
 #error {
   color: red;
   font-weight: bold;
