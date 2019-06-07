d7d4e098dac9de57f6f80c4eccc778bff4d2344e
roll1226 <tomo_yasu_4724@icloud.com>
2019-06-07 20:35:18 +0900

    add/swipe

--- pages/index.vue
+++ pages/index.vue
@@ -1,59 +1,33 @@
 <template>
-  <section class="container">
-    <nuxt-link to="/swipe">あ</nuxt-link>
-    <label
-      >住所検索
-      <input
-        v-model="zipcode"
-        type="text"
-        placeholder="郵便番号を入力"
-        @change="search"
-    /></label>
-
-    <!-- <br /> -->
-    <!-- <button @click="searchAddressInfo">住所自動入力</button> -->
-    <br />
-    <p v-if="zipcode === ''">
-      入力してね
-    </p>
-    <p v-else-if="msg == ''">住所 ： {{ addressData['fullAddress'] }}</p>
-    <p v-else id="error">
-      {{ msg }}
-    </p>
-  </section>
+  <div>
+    <swipe class="my-swipe">
+      <swipe-item class="slide1"></swipe-item>
+      <swipe-item class="slide2"></swipe-item>
+      <swipe-item class="slide3"></swipe-item>
+    </swipe>
+  </div>
 </template>
 
-<script>
-import { mapGetters } from 'vuex'
-
-export default {
-  data() {
-    return {
-      zipcode: ''
-    }
-  },
-  computed: {
-    ...mapGetters(['msg', 'addressData'])
-  },
-  methods: {
-    search() {
-      this.$store.dispatch('serchApi', this.zipcode)
-    }
-  }
-}
-</script>
-
 <style>
-.container {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  height: 100vh;
+.my-swipe {
+  height: 200px;
+  color: #fff;
+  font-size: 30px;
+  text-align: center;
 }
-#error {
-  color: red;
-  font-weight: bold;
+
+.slide1 {
+  background-color: #0089dc;
+  color: #fff;
+}
+
+.slide2 {
+  background-color: #ffd705;
+  color: #000;
+}
+
+.slide3 {
+  background-color: #ff2d4b;
+  color: #fff;
 }
 </style>
