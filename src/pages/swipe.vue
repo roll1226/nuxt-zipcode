63fa74e62795935438ee2cce0f6f239e6b5c6ad5
roll1226 <tomo_yasu_4724@icloud.com>
2019-06-10 20:28:56 +0900

    add/nuxt

--- src/pages/swipe.vue
+++ src/pages/swipe.vue
@@ -1,7 +1,10 @@
 <template>
   <div>
-    <div v-touch:swipe.left="navLeft" class="swipeLeft"></div>
-    <div v-touch:swipe.right="navRight" class="swipeRight"></div>
+    <div
+      v-touch:swipe.left="navLeft"
+      v-touch:swipe.right="navRight"
+      class="swipeLeft"
+    ></div>
   </div>
 </template>
 
@@ -21,17 +24,8 @@ export default {
 <style lang="scss">
 .swipeLeft,
 .swipeRight {
-  width: 50vw;
+  width: 100vw;
   height: 100vh;
   position: fixed;
-  top: 0;
-}
-
-.swipeLeft {
-  left: 0;
-}
-
-.swipeRight {
-  right: 0;
 }
 </style>
